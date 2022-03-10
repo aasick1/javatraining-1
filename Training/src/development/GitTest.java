d4aa71ef477c503631b280b77fcee88540317797
drpoudel3785 <dharmarajpoudel@hotmail.com>
2022-03-10 15:31:25 +0545

    GIT TEST CLASS ADDED

---
+++ Training/src/development/GitTest.java
@@ -0,0 +1,12 @@
+package development;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("We Are Learning GIT");
+
+	}
+
+}
